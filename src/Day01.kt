aba1cbfc420860917afa746e1a8a37afa135e84e
Paul <paul@bonugli.com>
2022-12-04 13:24:46 +0000

    days 1 & 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,11 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val fileContent = File("src/Day01.txt").readText()
+    val elves = fileContent.split("\n\n").map {
+        it.lines().sumOf { calories -> calories.toInt() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1: " + elves.max())
+    println("Part 2: " + elves.sortedDescending().take(3).sum())
 }
---
+++ src/Day02.kt
@@ -0,0 +1,104 @@
+enum class GameChoice() {
+    ROCK(),
+    PAPER(),
+    SCISSORS()
+}
+
+fun parseGameChoice(str: String): GameChoice {
+    return when (str) {
+        "A", "X" -> GameChoice.ROCK
+        "B", "Y" -> GameChoice.PAPER
+        "C", "Z" -> GameChoice.SCISSORS
+        else -> {
+            throw IllegalStateException("Unknown game choice $str")
+        }
+    }
+}
+
+fun scoreForChoice(choice: GameChoice): Int {
+    return choice.ordinal + 1
+}
+
+enum class GameOutcome(val score: Int) {
+    WIN(6),
+    DRAW(3),
+    LOSE(0)
+}
+
+fun part1(inputLines: List<String>) : Int {
+    val inputs = inputLines.map { parseGameChoice(it[0].toString()) to parseGameChoice(it[2].toString()) }
+
+    fun scoreForOutcome(theirChoice: GameChoice, myChoice: GameChoice): Int {
+        return when (myChoice) {
+            GameChoice.ROCK -> when (theirChoice) {
+                GameChoice.ROCK -> GameOutcome.DRAW
+                GameChoice.PAPER -> GameOutcome.LOSE
+                GameChoice.SCISSORS -> GameOutcome.WIN
+            }
+            GameChoice.PAPER -> when (theirChoice) {
+                GameChoice.ROCK -> GameOutcome.WIN
+                GameChoice.PAPER -> GameOutcome.DRAW
+                GameChoice.SCISSORS -> GameOutcome.LOSE
+            }
+            GameChoice.SCISSORS -> when (theirChoice) {
+                GameChoice.ROCK -> GameOutcome.LOSE
+                GameChoice.PAPER -> GameOutcome.WIN
+                GameChoice.SCISSORS -> GameOutcome.DRAW
+            }
+        }.score
+    }
+
+    val scores = inputs.map {
+        scoreForChoice(it.second) + scoreForOutcome(it.first, it.second)
+    }
+
+    return scores.sum()
+}
+
+fun part2(inputLines: List<String>) : Int {
+    fun parseOutcome(str: String): GameOutcome {
+        return when (str) {
+            "X" -> GameOutcome.LOSE
+            "Y" -> GameOutcome.DRAW
+            "Z" -> GameOutcome.WIN
+            else -> {
+                throw IllegalStateException("Unknown game outcome $str")
+            }
+        }
+    }
+
+    val inputs = inputLines.map { parseGameChoice(it[0].toString()) to parseOutcome(it[2].toString()) }
+
+    fun determineChoice(theirChoice: GameChoice, desiredOutcome: GameOutcome): GameChoice {
+        var idx = theirChoice.ordinal
+        if (desiredOutcome == GameOutcome.WIN) {
+            idx += 1
+
+            // 3 -> 0
+            if (idx > 2) {
+                idx -= 3
+            }
+        } else if (desiredOutcome == GameOutcome.LOSE) {
+            idx -= 1
+
+            // -1 -> 2
+            if (idx < 0) {
+                idx += 3
+            }
+        }
+
+        return GameChoice.values()[idx]
+    }
+
+    val scores = inputs.map {
+        scoreForChoice(determineChoice(it.first, it.second)) + it.second.score
+    }
+
+    return scores.sum()
+}
+
+fun main() {
+    val lines = readInput("Day02")
+    println("Part 1: " + part1(lines))
+    println("Part 2: " + part2(lines))
+}
