b658fc43597260df1c171c14a3e79ec70c59a078
Paul <paul@bonugli.com>
2022-12-04 14:10:16 +0000

    adding day 3

---
+++ src/Day03.kt
@@ -0,0 +1,29 @@
+fun main() {
+    fun determinePriority(c : Char) : Int {
+        return if (c > 'Z') {
+            c - 'a' + 1
+        } else {
+            c - 'A' + 26 + 1
+        }
+    }
+
+    fun part1(input : List<String>) : Int {
+        return input.map { line ->
+            val c1 = line.subSequence(0, line.length / 2)
+            val c2 = line.subSequence((line.length / 2), line.length)
+            c1 to c2
+        }.map { pair ->
+            pair.first.asIterable().first { pair.second.contains(it) }
+        }.sumOf { determinePriority(it) }
+    }
+
+    fun part2(input : List<String>) : Int {
+        return input.chunked(3).map { group ->
+            group[0].asIterable().first { group[1].contains(it) && group[2].contains(it) }
+        }.sumOf { determinePriority(it) }
+    }
+
+    val lines = readInput("Day03")
+    println("Part 1: " + part1(lines))
+    println("Part 2: " + part2(lines))
+}
